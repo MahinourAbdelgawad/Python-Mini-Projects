b0f46730c41e8e48e14f681d290aba6cb71c0c14
Mahinour Abdelgawad <MahinourAbdelgawad@aucegypt.edu>
2024-10-06 17:02:39 +0300

    cleaned up code, renamed project

--- Simple Youtube Video Downloader Tkinter/ytvideodownloader.py
+++ Simple Youtube Video Downloader Tkinter/ytvideodownloader.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import *
-# from pytube import YouTube
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 from tkinter import messagebox, filedialog
@@ -9,43 +8,42 @@ import threading
 def Widgets(): # to create tkinter widgets
     label = Label(main, text = "Youtube Video Downloader",
                   padx = 15, pady = 15,
-                  font = "Helvetica 12",
-                  bg = "red",
-                  fg = "white")
+                  font = "Arial 12",
+                  bg = "red", fg = "white")
     label.grid(row = 1, column = 1,
                pady = 10, padx = 5,
                columnspan = 3)
     
     linkLabel = Label(main, text = "Youtube Link:",
-                      bg = "red",
+                      bg = "red", fg = "white",
                       pady = 5, padx = 5)
     linkLabel.grid(row = 2, column = 0,
                    pady = 5, padx = 5)
     
     main.linkText = Entry(main, width = 35,
                           textvariable = link,
-                          font = "Helvetica 10")
+                          font = "Arial 10")
     main.linkText.grid(row = 2, column = 1,
                        pady = 5, padx = 5,
                        columnspan = 2)
     
     folderLabel = Label(main, text = "Save To:",
-                        bg = "red",
-                        pady = 5, padx = 9)
+                        bg = "red", fg = "white",
+                        pady = 5, padx = 5)
     folderLabel.grid(row = 3, column = 0,
                      pady = 5, padx = 5)
     
-    main.folderText = Entry(main, width = 27,
+    main.folderText = Entry(main, width = 35,
                             textvariable = path,
-                            font = "Helvetica 10")
+                            font = "Arial 10")
     main.folderText.grid(row = 3, column = 1,
                         pady = 5, padx = 5)
     
     browseButton = Button(main, text = "Browse",
                           command = Browse, 
                           width = 10,
-                          bg = "red",
-                          font = "Helvetica, 10",
+                          bg = "red", fg = "white",
+                          font = "Arial, 10",
                           relief = GROOVE)
     browseButton.grid(row = 3, column = 2,
                       pady = 1, padx = 1)
@@ -53,9 +51,9 @@ def Widgets(): # to create tkinter widgets
     downloadButton = Button(main, text = "Download Video",
                             command = Download,
                             width = 20,
-                            bg = "red",
+                            bg = "red", fg = "white",
                             pady = 10, padx = 15,
-                            font = "Helvetica, 10",
+                            font = "Arial, 10",
                             relief = GROOVE)
     downloadButton.grid(row = 4, column = 1,
                         pady = 20, padx = 20)
@@ -66,19 +64,15 @@ def Browse(): # to select folder
     path.set(directory)
 
 def Download(): # to download video to selected folder
-    print("clicked download")
     yt_link = link.get()
     folder = path.get()
 
     if yt_link and folder:
         threading.Thread(target = downloadVideo, args = (yt_link, folder)).start()
-        print("in if")
     else:
         messagebox.showwarning("Please enter a link and select a download path")
-        print("in else")
 
 def downloadVideo(yt_link, folder):
-    print("in download video")
     try:
         getVideo = YouTube(yt_link, on_progress_callback = on_progress)
         print(getVideo.title)
